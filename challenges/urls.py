6bb67afef3dc69a9c6f85d13626b0e2d5a9ad215
Daniel <nicorici73@gmail.com>
2025-10-09 14:25:54 +0300

    added reverse function for redirecting to the right path

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns =[
-    path("january",views.index),
+    path("<int:month>",views.monthly_challange_by_number),
+    path("<str:month>",views.monthly_challange,name="monthly_challanges"),
+
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+monthly_challanges= {
+    "january": "Work harder!",
+    "february": "Keep the momentum!",
+    "march": "Stay consistent!",
+    "april": "Push through challenges!",
+    "may": "Keep growing!",
+    "june": "Halfway there, stay strong!",
+    "july": "Don’t lose focus!",
+    "august": "Keep improving!",
+    "september": "Push your limits!",
+    "october": "Stay disciplined!",
+    "november": "Finish strong!",
+    "december": "Reflect and prepare for next year!"
+}
 
 def index(request):
     return HttpResponse("This works!")
+
+def monthly_challange_by_number(request, month):
+    try:
+        path=reverse("monthly_challanges",args=[list(monthly_challanges.keys())[month-1]])
+        return HttpResponseRedirect(path)
+    except:
+        return HttpResponseNotFound("choose a number of the month")
+
+
+def monthly_challange(request, month):
+    try:
+        return HttpResponse(monthly_challanges[month])
+    except:
+        return HttpResponseNotFound("choose the month")
